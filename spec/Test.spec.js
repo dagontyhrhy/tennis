a1e9963e190fe810e28ce58a7494b00f90a4db9b
dagontyhrhy <takecom05@gmail.com>
2016-09-27 21:37:35 +0700

    tennis

--- spec/Test.spec.js
+++ spec/Test.spec.js
@@ -1,5 +1,38 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-});
+
+function scoreA(){
+this.score = scoreA;
+this.getscore = function()
+{
+  return scoreA+15;
+}
+if(scoreA === 40){
+  console.log("Player A won");
+}
+else{
+  echo.
+}
+}
+
+function scoreB(){
+this.score = scoreB;
+this.getscore = function()
+{
+  return scoreB+15;
+}
+if(scoreB === 40){
+  console.log("Player B won");
+}
+else{
+  echo.
+}
+}
+
+var score = new score();
+var get;
+รับค่า A,B;
+if(get==='A'){
+scoreA.score();
+}
+else if(get==='B'){
+scoreB.score();
+}
